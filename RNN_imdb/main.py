6b323e59f45052c2a3f30b9e1b1bec98ad4737da
MAMIDI LOKESH VENKATA SAI <36584697+lokesh-venkata-sai@users.noreply.github.com>
2024-10-09 16:55:21 -0400

    Update main.py

--- RNN_imdb/main.py
+++ RNN_imdb/main.py
@@ -11,7 +11,7 @@ word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
 
 # Load the pre-trained model with ReLU activation
-model = load_model('models/simple_rnn_imdb.h5')
+model = load_model('/models/simple_rnn_imdb.h5')
 
 # Step 2: Helper Functions
 # Function to decode reviews
